1b858a21e3f0e60dcd69ab881d9d66e54e91c776
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 16:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories() {
   console.log(this.responseText)
   let repoList = '<ul>'
   for (var i = 0; i< this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>'
+    repoList += ('<li>' + this.responseText[i]['name'] + '</li>')
   }
   repoList += '</ul>'
   document.getElementById('repositories').innerHTML = repoList

d430564416eca24c0678f498ad55dc6c8077a5dc
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 16:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function showRepositories() {
   console.log(this.responseText)
-  
+
 }
 
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories())
+  req.addEventListener('load', showRepositories
   req.open('GET', 'https://api.github.com/users/octocat/repos')
   req.send();
 }

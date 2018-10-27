bd5a15d27742f551c95b30b09a5b877f91086906
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 16:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function showRepositories() {
   console.log(this.responseText)
-
+  let repoList = '<ul>'
+  for (var i = 0; i< this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>'
+  }
+  let repoList += '</ul>'
+  document.getElementById('repositories').innerHTML = repoList
 }
 
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories
+  req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/octocat/repos')
   req.send();
 }

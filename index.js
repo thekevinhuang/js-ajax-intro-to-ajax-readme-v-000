afe2fc76f0ce98f9170eef4dd897277a892dca81
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 16:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function showRepositories() {
   var repos=JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r=> '<li>'+r.name+'</li>').join(' ')}</ul>`;
+  const repoList = `<ul>${repos
+    .map(
+      r=> 
+      '<li>'+
+      r.name+
+      ' - <a href="#" data-repo="'+
+      r.name+
+      '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+      .join(' ')}</ul>`;
 
   document.getElementById('repositories').innerHTML = repoList
 }
